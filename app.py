4809b1ed637c20e2d680739c4f5aceaf83094903
wj9183 <wj9183@daum.net>
2021-04-27 16:54:42 +0900

    unit test

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+# from menu.introduce import introduce
+# from menu.ssd import ssd_image
+# from menu.yolo import yolo
+# from menu.semantic_segmentation import semantic_segmentation
+
+def main():
+    # menu = ['Object Detection', 'SSD', 'YOLO', 'Semantic Segmentation']
+    # choice = st.sidebar.selectbox("메뉴", menu)
+    
+    
+    # if choice == 'Object Detection':
+    #     introduce()
+    # elif choice == 'SSD':
+    #     menu = ['Object Detection', '이미지', '동영상']
+    #     choice = st.sidebar.selectbox("메뉴", menu)
+    #     if choice == '이미지':
+    #         ssd_image()
+    #     elif choice == '동영상':
+    #         # ssd_video()
+    #         pass
+    # elif choice == 'YOLO':
+    #     yolo()
+    
+    # elif choice == 'Semantic Segmentation':
+    #     semantic_segmentation()
+    
+    # else:
+    #     #메뉴를 잘못 선택한 경우 에러코드를 반환합니다.
+    #     {"err_code" : 0}
+    pass
+
+
+if __name__ == '__main__':
+    main()
